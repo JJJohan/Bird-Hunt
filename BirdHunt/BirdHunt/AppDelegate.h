3b9fc9486e0a61174d6a7a01df87fbe2c785754c
Johan Rensenbrink <jjjohan@optusnet.com>
2011-09-11 17:25:03 +1000

    Initial commit.

---
+++ BirdHunt/BirdHunt/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  BirdHunt
+//
+//  Created by Johan Rensenbrink on 11/09/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ BirdHunt/BirdHunt/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BirdHunt
+//
+//  Created by Johan Rensenbrink on 11/09/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
